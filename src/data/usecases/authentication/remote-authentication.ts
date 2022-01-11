15bedcbd76872cf2e245115c976c8b453492d29b
Felipe Santos <rfelipe273@gmail.com>
2022-01-11 17:26:00 -0300

    feat: ensure RemoteAuthentication return an Account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,7 +3,7 @@ import { Account } from "@/domain/entities/account";
 import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors";
 import { Authentication } from "@/domain/usecases/authentication";
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<
@@ -11,14 +11,14 @@ export class RemoteAuthentication {
       Account
     >
   ) {}
-  async auth(params: Authentication.Params): Promise<void> {
+  async auth(params: Authentication.Params): Promise<Account> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params,
     });
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break;
+        return httpResponse.body;
       case HttpStatusCode.UNAUTHORIZED:
         throw new InvalidCredentialsError();
       default:
