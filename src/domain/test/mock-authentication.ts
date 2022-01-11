543b6b50488041436a5362974b79587d8dc067f1
Felipe Santos <rfelipe273@gmail.com>
2022-01-11 16:07:35 -0300

    refactor: change imports to use module

--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,5 +1,5 @@
 import faker from "@withshepherd/faker";
-import { Authentication } from "../usecases/authentication";
+import { Authentication } from "@/domain/usecases/authentication";
 
 export const mockAuthentication = (): Authentication.Params => ({
   email: faker.internet.email(),
